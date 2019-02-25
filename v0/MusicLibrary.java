be04c74e583e01bed92a631de8a08c91f12be683
Jakob Wyatt <jakob.wyatt@gmail.com>
2019-02-25 15:53:14 +0800

    added initial version of musiclibrary head file to repo

---
+++ v0/MusicLibrary.java
@@ -0,0 +1,21 @@
+// FILE: MusicLibrary.java
+// AUTHOR: Jakob Wyatt
+// UNIT: OOPD
+// PURPOSE: Entry point of Music Library program.
+//              Includes high level exception handling.
+// REFERENCES: None
+// COMMENTS: None
+// REQUIRES: UserInterface.java
+// Last Mod: 13/09/2018
+// Modification history:
+//  None
+
+import java.util.*;
+
+public class MusicLibrary
+{
+    public static void main ( String[] args )
+    {
+        UserInterface.menu();
+    }
+}
